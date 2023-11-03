4042ea7bc3a756ba769fc397220b1b13bc14207d
Gabriel Francisco <77128754+gabrielfgt@users.noreply.github.com>
2023-11-03 18:18:37 -0300

    Add files
    initial commit

---
+++ Weather App/Weather App/Color+Extensions.swift
@@ -0,0 +1,15 @@
+//
+//  Color+Extensions.swift
+//  Weather App
+//
+//  Created by Gabriel Francisco on 03/11/23.
+//
+
+import Foundation
+import UIKit
+
+//extension UIColor {
+//    static let contrastCollor = UIColor(named: "contrastCollor")
+//    static let colorBlue = UIColor(named: "colorBlue")
+//    static let softGray = UIColor(named: "softGray")
+//}
